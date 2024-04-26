68502c4f16cb2b3f146c8cbb57763bd12cbdc9e5
Harries-Gavtham <harriesjj77@gmail.com>
2024-04-26 06:13:03 +0530

    Header completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="py-6 mb-12 border-b">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link>
+          <img src={logo} alt="Logo" />
+        </Link>
+        <div className="flex items-center gap-6">
+          <Link className="hover:text-violet-900 transition" to="">
+            Log in
+          </Link>
+          <Link
+            className="bg-violet-700 text-white hover:bg-violet-800 px-4 py-2 rounded-lg transition"
+            to=""
+          >
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
