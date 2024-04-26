b2bcab53fcf32bd232aa596db829d0b1f5029b1f
Harries-Gavtham <harriesjj77@gmail.com>
2024-04-26 16:54:11 +0530

    HouseList completed

--- src/components/HouseList.jsx
+++ src/components/HouseList.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { HouseContext } from "./HouseContext";
+import { Link } from "react-router-dom";
+import House from "./House";
 
 const HouseList = () => {
-  return <div>HouseList</div>;
+  const { houses, loading } = useContext(HouseContext);
+  return (
+    <section className="mb-20">
+      <div className="container mx-auto">
+        <div>
+          {houses.map((house, index) => {
+            return (
+              <Link to={`/property/${house.id}`} key={index}>
+                <House house={house} />
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HouseList;
