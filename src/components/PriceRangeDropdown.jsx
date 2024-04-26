2586f3c5d0b61456398b2911a0242930313a7603
Harries-Gavtham <harriesjj77@gmail.com>
2024-04-26 16:27:10 +0530

    PriceRangeDropDown Completed

--- src/components/PriceRangeDropdown.jsx
+++ src/components/PriceRangeDropdown.jsx
@@ -18,8 +18,22 @@ const PriceRangeDropdown = () => {
     {
       value: "100000 - 130000",
     },
-    
-  ]
+    {
+      value: "130000 - 160000",
+    },
+    {
+      value: "160000 - 190000",
+    },
+    {
+      value: "190000 - 220000",
+    },
+    {
+      value: "10000 - 30000",
+    },
+    {
+      value: "30000 - 40000",
+    },
+  ];
 
   return (
     <Menu as="div" className="dropdown relative">
@@ -29,9 +43,7 @@ const PriceRangeDropdown = () => {
       >
         <RiWallet3Line className="dropdown-icon-primary" />
         <div className="">
-          <div className="text-[15px] font-medium leading-tight">
-            {property}
-          </div>
+          <div className="text-[15px] font-medium leading-tight">{price}</div>
           <div className="text-[13px]">Choose price range</div>
         </div>
         {isOpen ? (
@@ -41,15 +53,15 @@ const PriceRangeDropdown = () => {
         )}
       </Menu.Button>
       <Menu.Items className="dropdown-menu">
-        {properties.map((property, index) => {
+        {prices.map((price, index) => {
           return (
             <Menu.Item
-              onClick={() => setProperty(property)}
+              onClick={() => setPrice(price.value)}
               className="cursor-pointer hover:text-violet-700 transition"
               as="li"
               key={index}
             >
-              {property}
+              {price.value}
             </Menu.Item>
           );
         })}
