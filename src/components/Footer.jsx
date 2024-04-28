dc6eaf3a6080e53554dc4e932337034b7c05c978
Harries-Gavtham <harriesjj77@gmail.com>
2024-04-28 11:11:40 +0530

    Fixed the houselist margin and footer bg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-black py-8 text-center text-white">
+    <footer className="bg-[#212427] py-5 text-center text-white">
       <div className="container mx-auto">
-          Copyright &copy; 2024. All rights reserved.
+        Copyright &copy; 2024. All rights reserved.
       </div>
       <p>Developed by HARRIES GAVTHAM</p>
     </footer>
